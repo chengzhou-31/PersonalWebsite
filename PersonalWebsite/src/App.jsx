8358b25d34e94f236a587bd65f08947217b4a288
Cheng Zhou <chzh3458@colorado.edu>
2022-12-14 19:20:12 -0700

    Initial Commit

---
+++ PersonalWebsite/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import NavBar from './partials/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App
---
+++ PersonalWebsite/src/partials/navbar.jsx
@@ -0,0 +1,9 @@
+function NavBar(){
+    return (
+        <nav className="bg-gray-800">
+            <a href="/pages/home">Home</a>
+        </nav>
+    );
+}
+
+export default NavBar;
